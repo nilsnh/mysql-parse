a7de9998aeff972bd722016e405a5869173ba168
Nils Norman Haukås <nils@thunki.com>
2018-05-27 18:28:22 +0200

    support piping input to command

--- index.js
+++ index.js
@@ -2,15 +2,32 @@
 'use strict'
 
 const { buildMysqlParams, parseUri } = require('./lib')
+const readline = require('readline')
 
+// run when called as command like 'node index.js'
 if (require.main === module) {
-  // run when called as command
+  // handle being called like so 'mysql-param <connection-string>'
   const uri = process.argv[2]
-  if (!uri) {
-    console.error('Error. Please provide a mysql connection string.')
-    process.exit(1)
+  if (uri) {
+    process.stdout.write(buildMysqlParams(uri))
+    process.exit(0)
   }
-  process.stdout.write(buildMysqlParams(uri))
+  // give user two seconds to pipe a connection string before giving up
+  const timeRef = setTimeout(() => {
+    if (!uri) {
+      console.error('Error. Please provide a mysql connection string.')
+      process.exit(1)
+    }
+  }, 1000)
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+  })
+  // handle being called like so 'echo <connection-string> | mysql-param'
+  rl.on('line', line => {
+    clearTimeout(timeRef)
+    process.stdout.write(buildMysqlParams(line))
+  })
 } else {
   // run when included as a module
   module.exports = {
